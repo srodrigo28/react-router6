0b4b41855ce8dbf56528a10b437bda408cfc1b58
Sebastiao <rodrigoexer1@gmail.com>
2023-06-26 07:08:32 -0300

    correção global styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 
-export const GlobalStyles = createGlobalStyle `
+export const GlobalStyles = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
